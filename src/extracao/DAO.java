4245acc013c5cde589db59e07eb02b09c03daf8a
JoelGama <joelgama2015@gmail.com>
2019-01-12 19:17:11 +0000

    Class LivroDAO

---
+++ src/extracao/DAO.java
@@ -0,0 +1,11 @@
+package extracao;
+
+import java.sql.Connection;
+
+public class DAO {
+    protected Connection connection;
+
+    public DAO(Connection c) {
+        this.connection = c;
+    }
+}
---
+++ src/extracao/LivroDAO.java
@@ -0,0 +1,54 @@
+package extracao;
+
+import java.util.HashSet;
+import java.util.Set;
+import transformacao.*;
+import java.sql.*;
+
+
+public class LivroDAO extends DAO {
+
+    public LivroDAO(Connection c) {
+        super(c);
+    }
+
+    public Set<Integer> keySet() throws SQLException {
+        HashSet<Integer> r = new HashSet<>();
+
+        Statement s = this.connection.createStatement();
+        ResultSet rs = s.executeQuery("select Id from biblioteca.livro;");
+
+        while (rs.next()) {
+            r.add(rs.getInt("Id"));
+        }
+
+        rs.close();
+        s.close();
+
+        return r;
+    }
+
+    public Livro get(int id) throws SQLException {
+        Livro livro = null;
+
+        // get nome, resumo e data
+        PreparedStatement p = this.connection.prepareStatement("select * from biblioteca.livro where Id = ?;");
+        ResultSet r = null;
+        p.setInt(1, id);
+        try {
+            r = p.executeQuery();
+            if (r.next()) {
+                livro = new Livro(r.getNome("Titulo"),
+                                  r.getResumo("Resumo"),
+                                  r.getTimestamp("DataPublicacao").toLocalDateTime());
+            } else {
+                throw new SQLException("biblioteca.livro is empty.");
+            }
+        } finally {
+            if (r != null) r.close();
+            p.close();
+        }
+
+        return cliente;
+    }
+}
